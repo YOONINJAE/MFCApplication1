7f4159371ff061918ce217d85f5c343b832b217e
윤인재 <@LAPTOP-C3KU7RBT>
2022-01-12 22:57:02 +0900

    초기 커밋.

---
+++ MFCApplication1Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// MFCApplication1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCApplication1Dlg 대화 상자
+class CMFCApplication1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplication1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	char m_selected_op; //   사용자가 어떤 연산자를 사용하는지 구분하는 척도
+	char m_clear_num_flag; // 사용자가 연산자를 클릭했는지 여부를 따지는 척도
+	int m_first_num, m_second_num; // 숫자 두개
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton12();
+	CString result;
+};
---
+++ resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication1.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_1                           1002
+#define IDC_EDIT2                       1003
+#define IDC_EDIT3                       1004
+#define IDC_2                           1009
+#define IDC_3                           1010
+#define IDC_4                           1011
+#define IDC_6                           1012
+#define IDC_BUTTON7                     1013
+#define IDC_5                           1014
+#define IDC_Substract                   1015
+#define IDC_0                           1016
+#define IDC_8                           1017
+#define IDC_Plus                        1018
+#define IDC_Divide                      1019
+#define IDC_Multiply                    1020
+#define IDC_9                           1021
+#define IDC_Clear                       1022
+#define IDC_Equal                       1023
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
